f6431e1b759ed43bffa41068a1851492f2907e11
laurens <laurens.le.jeune0@gmail.com>
2019-05-30 20:07:31 +0200

    added documentation in about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -13,11 +13,19 @@
     <p>Obligatory addition (because of the used translation service):</p>
     <a href="https://translate.yandex.com">Powered by Yandex.Translate</a>
     <br>
-    <h4>Created services (besides the REST service in Laravel, which also includes the database).</h4>
+    <h4>Created services</h4>
     <ul>
-        <li><a href="http://lyricsanalyzerapi.westeurope.azurecontainer.io/documentation">Lyrics analyzer REST api in Flask, Python</a></li>
-        <li><a href="http://hasherapi.westeurope.azurecontainer.io/">Hash function REST api in Node.JS</a></li>
-        <li><a href=".">Translation SOAP api in C#</a>
+        <li><a href="http://laurenslaravelwebapp.azurewebsites.net/api/documentation">Database API</a>->
+            <a href="https://github.com/laurenslejeune/soacl_laravel">Github</a></li>
+        <li><a href="http://lyricsanalyzerapi.westeurope.azurecontainer.io/documentation">Lyrics analyzer REST api in Flask, Python</a>->
+            <a href="https://github.com/laurenslejeune/lyricsanalyzer_api">Github</a></li>
+        <li><a href="http://hasherapi.westeurope.azurecontainer.io/">Hash function REST api in Node.JS</a>->
+            <a href="https://github.com/laurenslejeune/soacl_hasher">Github</a></li>
+        <li><a href="https://lyricstranslator.azurewebsites.net/LyricsProcessor.asmx">Translation SOAP api in C#</a>-> 
+            <a href="https://github.com/laurenslejeune/soacl_soap">Github</a></li>
     </ul>
+    <br>
+    <h4>MySQL Docker container</h4>
+    <ul><a href="https://github.com/laurenslejeune/soacl_mysql_container">Configuration</a></ul>
 </div>
 @stop
